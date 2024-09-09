2fcb4a334013927944fb1c1bc99f5bf9ed068a56
Wonseok Yang <deadline.yang@gmail.com>
2024-09-09 18:12:18 +0900

    feat: Add Custom Operator for more logging on GlueJobOperator

---
+++ apache-airflow/custom-providers/operators/glue.py
@@ -0,0 +1,9 @@
+from airflow.providers.amazon.aws.operators.glue import GlueJobOperator
+
+
+class GlueJobWithLoggingOperator(GlueJobOperator):
+    def pre_execute(self, context):
+        logging.info("Preparing to run Glue Job with params")
+        logging.info("\t Job Name: {self.job_name}")
+        logging.info("\t Script Args: {self.script_args}")
+        logging.info("\t Job Run kwargs: {self.run_job_kwargs}")
