f46ab5f305af2f5c84166275c36d038081a17bf6
dead-1ine <yang991211@naver.com>
2024-10-13 10:59:35 +0000

    todo: Add todo comments

--- apache-airflow/custom-providers/operators/glue.py
+++ apache-airflow/custom-providers/operators/glue.py
@@ -3,8 +3,13 @@ import logging
 from airflow.providers.amazon.aws.operators.glue import GlueJobOperator
 
 class GlueJobWithLoggingOperator(GlueJobOperator):
-    def pre_execute(self, context=None):
+    def pre_execute(self, context):
         logging.info("Preparing to run Glue Job with params")
         logging.info("\t Job Name: {self.job_name}")
         logging.info("\t Script Args: {self.script_args}")
         logging.info("\t Job Run kwargs: {self.run_job_kwargs}")
+
+    def execute(self, context):
+        super().execute()
+
+        # Check error message with AwsHook
