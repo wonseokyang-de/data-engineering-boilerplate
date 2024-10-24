6561ac26ee6b045edf9deb3f556901eeebcc295f
dead-1ine <yang991211@naver.com>
2024-10-24 13:21:23 +0000

    just commit

--- apache-spark/delta/main.py
+++ apache-spark/delta/main.py
@@ -2,7 +2,7 @@ from delta import *
 from pyspark.sql import SparkSession
 
 spark = (SparkSession.builder
-    .appName("default")
+    .appName('default')
     .config('spark.jars.packages', 'io.delta:delta-core_2.12:2.1.0')
     .config('spark.sql.extensions', 'io.delta.sql.DeltaSparkSessionExtension')
     .config('spark.sql.catalog.spark_catalog', 'org.apache.spark.sql.delta.catalog.DeltaCatalog')
@@ -13,7 +13,6 @@ data = [('Alice', 34), ('Bob', 45), ('Cathy', 29)]
 
 df = spark.createDataFrame(data, ['name', 'age'])
 
-(df.write
-    .format('delta')
- .save('./datalake/people')
+(df.write.format('delta')
+    .save('./datalake/people')
 )
