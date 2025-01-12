fb6942a55956d7302486fb62357b906e358c9066
wonseokyang-de <deadline.yang@gmail.com>
2025-01-12 15:56:51 +0900

    chore: Remove annotations and change DataFrame variable name

--- apache-spark/consume_kafka_message.py
+++ apache-spark/consume_kafka_message.py
@@ -2,13 +2,11 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import window, col
 
 
-# Spark 세션 생성
 spark = (SparkSession.builder
 	.appName("KafkaToS3Batch")
 	.getOrCreate()
 )
 
-# Kafka 소스로부터 스트리밍 데이터프레임 읽기
 df = (spark.readStream
 	.format("kafka")
 	.option("kafka.bootstrap.servers", "your_kafka_server:port")
@@ -17,17 +15,14 @@ df = (spark.readStream
 	.load()
 )
 
-# 데이터프레임을 문자열로 변환 (예제에서는 메시지의 값만 사용)
 df = df.selectExpr("CAST(value AS STRING)")
 
-# 30분 간격으로 윈도우 생성
-windowedCounts = (df
+window_count_df = (df
 	.groupBy(window(col("timestamp"), "30 minutes"))
     .count()
 )
 
-# 콘솔에 출력 (실제 운영에서는 이 부분을 S3에 쓰는 코드로 대체)
-query = (windowedCounts
+query = (window_count_df
 	.writeStream
 	.outputMode("complete")
 	.format("console")
