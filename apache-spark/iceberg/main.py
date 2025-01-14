70db3335c0a9b33ead3e4e91cf078b972feecd44
wonseokyang-de <deadline.yang@gmail.com>
2025-01-14 14:23:32 +0000

    feat: Create iceberg env

---
+++ apache-spark/iceberg/main.py
@@ -0,0 +1,17 @@
+from delta import *
+from pyspark.sql import SparkSession
+
+# TODO: Change Delta to Iceberg
+# spark = (SparkSession.builder .appName('default')
+#     .config('spark.jars.packages', 'io.delta:delta-core_2.12:2.1.0')
+#     .config('spark.sql.extensions', 'io.delta.sql.DeltaSparkSessionExtension')
+#     .config('spark.sql.catalog.spark_catalog', 'org.apache.spark.sql.delta.catalog.DeltaCatalog')
+#     .getOrCreate()
+# )
+#
+#
+#
+
+data = [('Alice', 34), ('Bob', 45), ('Cathy', 29)]
+
+df = spark.createDataFrame(data, ['name', 'age'])
